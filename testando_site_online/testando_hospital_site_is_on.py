a6f1a37b24a948911a939dca03606ef8d14552f2
eduardopeluchi <eduardopeluchi@outlook.com>
2021-01-10 14:00:08 -0300

    testando_if_site_is_on

---
+++ testando_site_online/testando_hospital_site_is_on.py
@@ -0,0 +1,7 @@
+import requests
+url = "https://www.conderg.org.br/"
+
+if requests.get(url).status_code == 200:
+    print("O servidor está disponível.")
+else:
+    print("O servidor está indisponível.")
